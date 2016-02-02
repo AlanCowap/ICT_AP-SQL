0e385ceae7d536f934365d75c6fec113456871d5
Alan Cowap <AlanCowap@users.noreply.github.com>
2016-02-02 09:40:46 +0000

    Update SampleSQL.sql

--- SampleSQL.sql
+++ SampleSQL.sql
@@ -1,5 +1,6 @@
 /*
-Let's create some Tables, relationships, and insert some data
+Let's CREATE some Tables, relationships, and insert some data.
+And now let's UPDATE and DELETE
 */
 Use SampleDB;
 
@@ -30,6 +31,23 @@ DOB DATE,
 Location_ID INT NOT NULL,
 FOREIGN KEY (Location_ID) REFERENCES Locations(Location_ID)
 );
+
 INSERT INTO People(First_Name, Last_Name, DOB, Location_ID)
-	VALUES('Richard', 'Feynman', '1957-01-31', 1);
+	VALUES('Richard', 'Feynman', '1957-01-31', 2);
 SELECT * FROM People;
+
+#UPDATE a Record
+UPDATE Locations SET City_Address='New York', Country='USA', Postcode='555';
+	#WHERE Location_ID=2;
+SELECT * FROM Locations;
+
+INSERT INTO People(First_Name, Last_Name, DOB, Location_ID)
+	VALUES('Isaac', 'Newton', '1642-01-31', 1);
+INSERT INTO People(First_Name, Last_Name, DOB, Location_ID)
+	VALUES('George', 'Boole', '1830-01-31', 1);
+    
+SELECT * FROM People;
+#Delete a Record
+DELETE FROM People WHERE First_Name = 'George' AND Last_Name = 'Boole';
+SELECT * FROM People;
+
